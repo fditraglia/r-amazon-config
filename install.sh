0b476abfb4ad9d15b69f805c4cab5df0fc0dcb1b
Francis DiTraglia <fditra@sas.upenn.edu>
2014-12-28 14:36:35 -0500

    update symlinking of dotfiles

--- install.sh
+++ install.sh
@@ -6,13 +6,6 @@ cd ~ # in case I run this from the wrong directory
 git clone https://github.com/fditraglia/dotfiles.git # my dotfiles
 git clone https://github.com/fditraglia/selectr.git # Bayesian variable selection
 
-
-# Symlink dotfiles from git repo
-ln -s ~/dotfiles/vimrc ~/.vimrc
-mv ~/.bashrc ~/.bashrc.bak
-ln -s ~/dotfiles/bashrc ~/.bashrc
-source .bashrc 
-
 # Install R (Assumes Ubuntu 14.04)
 sudo add-apt-repository "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" # ignore the error "sudo: unable to resolve host"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key E084DAB9
@@ -24,3 +17,11 @@ sudo chmod a+w /usr/local/lib/R/site-library
 
 # Install R packages
 R CMD BATCH install-r-packages.R
+
+# Symlink dotfiles from git repo
+ln -s ~/dotfiles/vimrc ~/.vimrc
+mv ~/.bashrc ~/.bashrc.bak
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/inputrc ~/.inputrc
+source ~/.bashrc 
+source ~/.inputrc 
