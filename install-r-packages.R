9727f8c0dec8d4488522ebec294b9ab1fe773e9f
Francis DiTraglia <fditra@sas.upenn.edu>
2014-12-28 22:23:40 -0500

    remove things I no longer need now that I have a simpler way of installing R

--- install-r-packages.R
+++
@@ -1,2 +0,0 @@
-install.packages("RcppArmadillo", dependencies = TRUE,
-                 repos = "http://cran.rstudio.com")
--- install.sh
+++ install.sh
@@ -7,24 +7,19 @@ git clone https://github.com/fditraglia/dotfiles.git # my dotfiles
 git clone https://github.com/fditraglia/selectr.git # Bayesian variable selection
 
 # Install R (Assumes Ubuntu 14.04)
-#sudo add-apt-repository "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" # ignore the error "sudo: unable to resolve host"
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 sudo apt-get update
 sudo apt-get install -y  r-base
 
 # Install R Packages
-sudo apt-get install -y r-rcpparmadillo
+sudo apt-get install -y r-cran-rcpp
+sudo apt-get install -y r-cran-rcpparmadillo
 
-# Make R packages directory writeable
-#sudo chmod a+w /usr/local/lib/R/site-library
 
-# Install R packages
-#R CMD BATCH ~/r-amazon-config/install-r-packages.R
 
 # Symlink dotfiles from git repo
 ln -s ~/dotfiles/vimrc ~/.vimrc
 mv ~/.bashrc ~/.bashrc.bak
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/inputrc ~/.inputrc
-source ~/.bashrc 
 source ~/.inputrc 
+source ~/.bashrc 
